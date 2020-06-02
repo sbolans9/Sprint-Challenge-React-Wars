b18e599c5f5cdc552c6c030956ae9d676de0930b
sergio bolanos <sergiobolanos1998@gmail.com>
2020-06-02 17:48:50 -0400

    added-data-to-root

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from 'react';
+
+function Character(props) {
+    return (
+        <div className='charactors'>
+            {props.characters.map((items) => {
+                return (
+                    <div>
+                        Name: {items.name} Height:{items.height} Mass: {items.mass} Eye Color: {items.eye_color} Gender: {items.gender} 
+                    </div>
+
+                )
+            })};
+        </div>
+    )
+};
+
+export default Character;
